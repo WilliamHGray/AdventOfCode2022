ffff36ec709d0fff5fd67e9196ab9cdd37c3100c
WilliamHGray <119584844+WilliamHGray@users.noreply.github.com>
2022-12-16 15:03:05 +0000

    Day 16 Part 1

---
+++ AdventOfCode16/AOC16.m
@@ -0,0 +1,42 @@
+tic
+A=fileread("AOC16.txt");
+B=regexp(A,'\n','split');
+for i=1:size(B,2)-1
+    B{i}=strtrim(B{i});
+end
+running = 0;
+names={};
+weights=[];
+cave=graph;
+numOfValves=i;
+j=1;
+for i=1:size(B,2)
+    f=regexp(B{i},' has','split');
+    f=regexp(f{1},' ','split');
+    g=regexp(B{i},';','split');
+    g=regexp(g{1},'=','split');
+    names(j)=f(2);
+    cave=addnode(cave,f(2));
+    weights(j)=str2num(g{2});
+    j=j+1;
+end
+
+for i=1:size(B,2)
+    f=regexp(B{i},'to valve','split');
+    f=regexp(f{2},', ','split');
+    if(f{1}(1)=='s')
+        f{1}=f{1}(3:4);
+    elseif(f{1}(1)==' ')
+        f{1}=f{1}(2:3);
+    end
+    for j=1:size(f,2)
+        if(~findedge(cave,names(i),f{j}))
+            cave=addedge(cave,names(i),f{j});
+        end
+    end
+end
+time=30;
+start='AA';
+index=find(strcmp(names,start));
+total=nextStep(time,index,cave,names,weights);
+toc
---
+++ AdventOfCode16/nextStep.m
@@ -0,0 +1,28 @@
+function[total]=nextStep(time,ind,caves,names,weights)
+if time~=0
+    max=0;
+    currNode=names{ind};
+    for i=find(weights)
+        if time==30
+            i
+        end
+        remainder=0;
+        nextNode=names{i};
+        q=distances(caves,currNode,nextNode);
+        if(q>0&&q<time)
+            t=time;
+            w=weights;
+            t=t-q-1;
+            remainder=t*weights(i);
+            w(i)=0;
+            remainder=remainder+nextStep(t,i,caves,names,w);
+        end
+        if max<remainder
+            max=remainder;
+        end
+    end
+    total=max;
+else
+    total=0;
+end
+end
